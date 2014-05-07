58b5970d0d8e17adf1a941846fc5e4d351964097
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-05-07 08:39:15 +0200

    Ejercicio sobre el manifiesto de Android

---
+++ AndroidManifest/src/com/arkaitzgarro/androidmanifest/MyReceiver.java
@@ -0,0 +1,19 @@
+package com.arkaitzgarro.androidmanifest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MyReceiver extends BroadcastReceiver {
+
+	public MyReceiver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onReceive(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
